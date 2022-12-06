943e5998bb4cd59bf37a73b572418ce901207a9d
HieuPT <hieupt26>
2022-12-06 14:00:06 +0700

    update

--- c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -4,15 +4,24 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { deleteTodo } from '../../businessLogic/todos'
+import { deleteTodo } from '../../helpers/todos'
 import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
-    // TODO: Remove a TODO item by id
+    // ##TODO: Remove a TODO item by id
+    const userId = getUserId(event)
+    console.log('Storing delete item: ', todoId)
+    await deleteTodo(userId, todoId)
     
-    return undefined
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({})
+    }
   }
 )
 
